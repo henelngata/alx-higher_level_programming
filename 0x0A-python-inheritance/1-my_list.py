b2f74515d32073f8de4b040ac9a30fe247fda780
henelngata <bucketpena013@gmail.com>
2024-04-23 20:38:44 +0300

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" This module contains a class MyList tha inherits
-from LIst
-"""
-
 
 class MyList(list):
     """This class MyList"""
 
     def print_sorted(self):
+        """orts and prints a list
+        Yeilds:
+            list: a asorted list
+        """
         my_list = list(self)
         my_list.sort()
         print("{}".format(my_list))
