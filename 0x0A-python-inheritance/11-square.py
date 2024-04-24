2acbf07df5dfe70c7a24ca5ba19b60f87cf18463
henelngata <bucketpena013@gmail.com>
2024-04-24 12:24:42 +0300

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module contains class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def area(self):
+        """returns the area of A square
+        """
+        return ((self.__size * self.__size))
