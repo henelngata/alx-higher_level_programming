dfd1a0a97f5f1b82091fb468f78ac98857037edc
henelngata <bucketpena013@gmail.com>
2024-04-24 11:29:24 +0300

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Conatains an empty class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle (BaseGeometry):
+    """
+    An empty class
+    """
+    def __init__(self, width, height):
+        """
+        args:
+            width: int the width
+            height: int the height
+        """
+        try:
+            super().integer_validator("width", width)
+            super().integer_validator("height", height)
+        except Exception as e:
+            print("[{}] {}".format(e.__class__.__name__, e))
+        else:
+            self.__width = width
+            self.__height = height
+
+    def area(self):
+        """Calculates the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns:
+        the string rep of this class
+        """
+        return "{} {}/{}".format(self.__class__.__name__,
+                                 self.__width,
+                                 self.__height)
