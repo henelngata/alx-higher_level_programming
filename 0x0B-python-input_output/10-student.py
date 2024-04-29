914c4d9eac067526f3efbe723458505a3426aec7
henelngata <bucketpena013@gmail.com>
2024-04-29 09:55:29 +0300

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""a class Student that defines a student by:
+"""
+
+
+class Student:
+    """a class Student that defines a student by:
+    Args:
+        first_name(string)
+        last_name(string)
+        age(int)
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student
+        Returns:
+                Dictionary
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            atts = {}
+
+            for att in attrs:
+                try:
+                    atts.update({att: getattr(self, att)})
+                except AttributeError:
+                    pass
+            return atts
