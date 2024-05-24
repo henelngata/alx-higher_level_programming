08c79de5e0a91d14f06f849166b2769d967de1b4
henelngata <bucketpena013@gmail.com>
2024-05-24 04:44:49 +0300

    ID can't be null UNIQUE

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
--- creates the table id_not_null
+-- creates the table unique_id
 
-CREATE TABLE IF NOT EXISTS id_not_null(
+CREATE TABLE IF NOT EXISTS unique_id(
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256) NOT NULL
 );
