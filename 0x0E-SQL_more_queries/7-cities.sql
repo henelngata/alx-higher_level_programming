e465db2fd52e9d01c5eaaba490536b843d807448
henelngata <bucketpena013@gmail.com>
2024-05-24 05:02:39 +0300

    ID can't be null UNIQUE

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
-    PRIMARY KEY (id)
 );
