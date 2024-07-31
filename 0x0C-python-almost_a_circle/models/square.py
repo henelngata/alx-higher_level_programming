ed62713c6c36e7efb3c195e14c5ba1c385ab7252
henelngata <bucketpena013@gmail.com>
2024-07-31 11:44:04 +0300

    Update the class Square by adding the public method update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,12 +39,11 @@ class Square(Rectangle):
                 self.id = args[0]
             if len(args) > 1:
                 self.width = args[1]
+                self.height = args[1]
             if len(args) > 2:
-                self.height = args[2]
+                self.x = args[2]
             if len(args) > 3:
-                self.x = args[3]
-            if len(args) > 4:
-                self.y = args[4]
+                self.y = args[3]
         else:
             if 'id' in kwargs:
                 self.id = kwargs['id']
