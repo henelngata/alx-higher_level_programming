f6ca9455426b5117016e4e90a2ac76049abf3d5d
henelngata <bucketpena013@gmail.com>
2024-07-31 13:58:16 +0300

    Update the class Base by adding the class method def create(cls, **dictionary)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,9 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(1, 2, 2)
+        dummy.update(**dictionary)
+        return dummy
