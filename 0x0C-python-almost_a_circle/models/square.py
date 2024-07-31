abb01b47c2692f1e9f1c7edfd3470d192a4947f3
henel ngata <bucketpena013@gmail.com>
2024-07-31 08:21:14 +0000

    break

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Imports class Rectangle from Rectangle module"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Class square inherites its attributes and behavior form rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self) -> str:
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y)
