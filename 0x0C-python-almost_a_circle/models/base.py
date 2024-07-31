97b1ff99f20a330e783839e2782929b14580756b
henelngata <bucketpena013@gmail.com>
2024-07-31 13:44:26 +0300

    Update the class Base by adding the static method def from_json_string(json_string)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,11 @@ class Base:
                 file.write(json_string)
         except IOError as e:
             print(f"Error: {e}")
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
