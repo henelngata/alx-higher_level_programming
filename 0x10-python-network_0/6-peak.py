de1052a708246b0409a703de9e232073efeaad39
henelngata <bucketpena013@gmail.com>
2024-08-08 17:06:10 +0300

    update folder 0x10

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+"""Contains a function"""
+
+def find_peak(list_of_integers):
+    """a function that finds a peak in a list of unsorted integers."""
+    if len(list_of_integers) > 0:
+        sorted_list = sorted(list_of_integers, reverse=True)
+        
+        return sorted_list[0]
+    else:
+        return "None"        
