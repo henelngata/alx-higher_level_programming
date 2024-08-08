b03e26ac9f4caae76114679850e690f510e19117
henelngata <bucketpena013@gmail.com>
2024-08-08 11:38:45 +0300

    kl

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+using mysqldb to querry a database and return results of a table
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    ps = sys.argv[2]
+    dbase = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306, user=name, passwd=ps,
+                         database=dbase)
+    cur = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name FROM cities
+               INNER JOIN states ON cities.state_id=states.id
+                ORDER BY cities.id ASC"""
+
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
