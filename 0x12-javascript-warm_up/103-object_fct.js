9dcb35ae86f8de1162fdadcdd6d1fc8ad8e097e6
henelngata <bucketpena013@gmail.com>
2024-08-09 12:46:36 +0300

    Increment

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr: function () {
+    this.value++;
+  }
 };
+
 console.log(myObject);
-myObject.incr = function incr () {
-  myObject.value += 1;
-};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
