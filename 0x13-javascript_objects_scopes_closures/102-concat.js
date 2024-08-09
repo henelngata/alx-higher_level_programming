e95f6ccb53b1683c0f50c80009841af3ec09efce
henelngata <bucketpena013@gmail.com>
2024-08-09 13:27:08 +0300

    Concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,4 +8,4 @@ const fileC = process.argv[4];
 const contentA = fs.readFileSync(fileA, 'utf-8');
 const contentB = fs.readFileSync(fileB, 'utf-8');
 
-fs.writeFileSync(fileC, contentA + '\n' + contentB);
+fs.writeFileSync(fileC, contentA + contentB);
