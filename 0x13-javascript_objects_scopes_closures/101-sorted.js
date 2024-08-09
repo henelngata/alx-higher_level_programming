61fbb648d969a99a430437edbc77e75af6af0e9b
henelngata <bucketpena013@gmail.com>
2024-08-09 13:21:09 +0300

    Sorted

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+for (const [userId, occurrence] of Object.entries(dict)) {
+  if (newDict[occurrence] === undefined) {
+    newDict[occurrence] = [];
+  }
+  newDict[occurrence].push(userId);
+}
+
+console.log(newDict);
