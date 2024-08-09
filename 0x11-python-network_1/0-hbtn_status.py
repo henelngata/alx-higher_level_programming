b180531cd7b821bde06aaaeff4dac82eb4839123
henelngata <bucketpena013@gmail.com>
2024-08-09 11:00:58 +0300

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    req = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
         html = response.read()
-    
+
     print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
