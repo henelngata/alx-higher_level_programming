123813c64b7937247628b056917d3e9d943ebe16
henelngata <bucketpena013@gmail.com>
2024-08-09 11:41:38 +0300

    Hello

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,11 +4,22 @@
 import requests
 import sys
 
+
+def get_commits(repo_name, owner_name):
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"""Statuscode: {response.status_code}""")
+
+
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(sys.argv[1], sys.argv[2]))
-    resp = r.json()
-    for responce in resp:
-        sha = responce.get("sha")
-        name = responce["commit"]["author"]["name"]
-        print("{}: {}".format(sha, name))
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    get_commits(repo_name, owner_name)
